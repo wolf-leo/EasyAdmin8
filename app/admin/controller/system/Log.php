8b530a18342d9305eb45c644e66d15c1be556d6d
wolfcode <37436228+wolf-leo@users.noreply.github.com>
2023-06-30 01:23:00 +0800

    Update Log.php

--- app/admin/controller/system/Log.php
+++ app/admin/controller/system/Log.php
@@ -32,27 +32,13 @@ class Log extends AdminController
                 return $this->selectList();
             }
             [$page, $limit, $where, $excludeFields] = $this->buildTableParams(['month']);
-
             $month = (isset($excludeFields['month']) && !empty($excludeFields['month']))
-                ? date('Ym',strtotime($excludeFields['month']))
+                ? date('Ym', strtotime($excludeFields['month']))
                 : date('Ym');
-
-            // todo TP6框架有一个BUG，非模型名与表名不对应时（name属性自定义），withJoin生成的sql有问题
-
-            $count = $this->model
-                ->setMonth($month)
-                ->with('admin')
-                ->where($where)
-                ->select();
-            $list = $this->model
-                ->setMonth($month)
-                ->with('admin')
-                ->where($where)
-                ->page($page, $limit)
-                ->order($this->sort)
-                ->select();
-
-            $data = [
+            $model = $this->model->setMonth($month)->with('admin')->where($where);
+            $count = $model->count();
+            $list  = $model->page($page, $limit)->order($this->sort)->select();
+            $data  = [
                 'code'  => 0,
                 'msg'   => '',
                 'count' => $count,
