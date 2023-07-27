0805c31921a1258f282dbc8098926f9cd82a18d1
wolfcode <37436228+wolf-leo@users.noreply.github.com>
2023-07-27 17:54:12 +0800

    Update index.js

--- public/static/admin/js/index.js
+++ public/static/admin/js/index.js
@@ -25,6 +25,28 @@ define(["jquery", "easy-admin", "echarts", "echarts-theme", "miniAdmin", "miniTa
                     })
                 });
             });
+            layui.form.on('switch(header-theme-mode)', function (data) {
+                let dark_mode = this.checked
+                let that = $('iframe').contents()
+                if (dark_mode) {
+                    $('#layuicss-theme-dark').attr({
+                        rel: "stylesheet",
+                        type: "text/css",
+                        href: "/static/admin/css/layui-theme-dark.css"
+                    })
+                        .appendTo("head");
+                    that.find("html").addClass('dark')
+                    $('html').addClass('dark')
+                } else {
+                    $('#layuicss-theme-dark').attr({
+                        rel: "stylesheet",
+                        type: "text/css",
+                        href: ""
+                    })
+                    that.find("html").removeClass('dark')
+                    $('html').removeClass('dark')
+                }
+            });
         },
         welcome: function () {
             miniTab.listen();
@@ -63,73 +85,75 @@ define(["jquery", "easy-admin", "echarts", "echarts-theme", "miniAdmin", "miniTa
             /**
              * 报表功能
              */
-            var echartsRecords = echarts.init(document.getElementById('echarts-records'), 'walden');
-            var optionRecords = {
-                title: {
-                    text: '访问统计'
-                },
-                tooltip: {
-                    trigger: 'axis'
-                },
-                legend: {
-                    data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
-                },
-                grid: {
-                    left: '3%',
-                    right: '4%',
-                    bottom: '3%',
-                    containLabel: true
-                },
-                toolbox: {
-                    feature: {
-                        saveAsImage: {}
-                    }
-                },
-                xAxis: {
-                    type: 'category',
-                    boundaryGap: false,
-                    data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
-                },
-                yAxis: {
-                    type: 'value'
-                },
-                series: [
-                    {
-                        name: '邮件营销',
-                        type: 'line',
-                        stack: '总量',
-                        data: [120, 132, 101, 134, 90, 230, 210]
+            $(function () {
+                let echartsRecords = echarts.init(document.getElementById('echarts-records'), 'walden');
+                let optionRecords = {
+                    title: {
+                        text: '访问统计'
                     },
-                    {
-                        name: '联盟广告',
-                        type: 'line',
-                        stack: '总量',
-                        data: [220, 182, 191, 234, 290, 330, 310]
+                    tooltip: {
+                        trigger: 'axis'
                     },
-                    {
-                        name: '视频广告',
-                        type: 'line',
-                        stack: '总量',
-                        data: [150, 232, 201, 154, 190, 330, 410]
+                    legend: {
+                        data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
                     },
-                    {
-                        name: '直接访问',
-                        type: 'line',
-                        stack: '总量',
-                        data: [320, 332, 301, 334, 390, 330, 320]
+                    grid: {
+                        left: '3%',
+                        right: '4%',
+                        bottom: '3%',
+                        containLabel: true
                     },
-                    {
-                        name: '搜索引擎',
-                        type: 'line',
-                        stack: '总量',
-                        data: [820, 932, 901, 934, 1290, 1330, 1320]
-                    }
-                ]
-            };
-            echartsRecords.setOption(optionRecords);
-            window.addEventListener("resize", function () {
-                echartsRecords.resize();
-            });
+                    toolbox: {
+                        feature: {
+                            saveAsImage: {}
+                        }
+                    },
+                    xAxis: {
+                        type: 'category',
+                        boundaryGap: false,
+                        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+                    },
+                    yAxis: {
+                        type: 'value'
+                    },
+                    series: [
+                        {
+                            name: '邮件营销',
+                            type: 'line',
+                            stack: '总量',
+                            data: [120, 132, 101, 134, 90, 230, 210]
+                        },
+                        {
+                            name: '联盟广告',
+                            type: 'line',
+                            stack: '总量',
+                            data: [220, 182, 191, 234, 290, 330, 310]
+                        },
+                        {
+                            name: '视频广告',
+                            type: 'line',
+                            stack: '总量',
+                            data: [150, 232, 201, 154, 190, 330, 410]
+                        },
+                        {
+                            name: '直接访问',
+                            type: 'line',
+                            stack: '总量',
+                            data: [320, 332, 301, 334, 390, 330, 320]
+                        },
+                        {
+                            name: '搜索引擎',
+                            type: 'line',
+                            stack: '总量',
+                            data: [820, 932, 901, 934, 1290, 1330, 1320]
+                        }
+                    ]
+                };
+                echartsRecords.setOption(optionRecords);
+                window.addEventListener("resize", function () {
+                    echartsRecords.resize();
+                });
+            })
         },
         editAdmin: function () {
             ea.listen();
