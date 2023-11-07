67f0c4ce53794c191bd501298ec35ea5eb5755b6
wolfcode <37436228+wolf-leo@users.noreply.github.com>
2023-11-07 11:10:05 +0800

    Create Index.php

---
+++ app/index/controller/Index.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\index\controller;
+
+use app\BaseController;
+use think\facade\Env;
+use think\response\Redirect;
+
+class Index extends BaseController
+{
+    /**
+     * @return Redirect
+     */
+    public function index(): Redirect
+    {
+        // 这是项目首页 系统默认跳转后台页面
+        return redirect('/' . Env::get('EASYADMIN.ADMIN', false));
+    }
+}
