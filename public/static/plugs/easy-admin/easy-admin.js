f7796fb422f55e1ca8def25fce23d16ac0424d01
wolfcode <37436228+wolf-leo@users.noreply.github.com>
2023-11-17 21:58:32 +0800

    Update Images Select.

--- public/static/plugs/easy-admin/easy-admin.js
+++ public/static/plugs/easy-admin/easy-admin.js
@@ -1454,11 +1454,8 @@ define(["jquery", "tableSelect", "ckeditor"], function ($, tableSelect, undefine
                                 });
                                 var url = urlArray.join(uploadSign);
                                 admin.msg.success('选择成功', function () {
-                                    let _dom = $(e).parents('.layui-form-item').find('input')
-                                    _dom.attr('value', url)
-                                    _dom.trigger("input")
-                                    // $(elem).val(url);
-                                    // $(elem).trigger("input");
+                                    $(elem).val(url);
+                                    $(elem).trigger("input");
                                 });
                             }
                         })
