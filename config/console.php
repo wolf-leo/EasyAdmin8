2ec046de07087329d6748f89a63c045f37db11a7
wolfcode <37436228+wolf-leo@users.noreply.github.com>
2024-05-14 13:33:30 +0800

    Update console.php

--- config/console.php
+++ config/console.php
@@ -3,7 +3,11 @@
 // | 控制台配置
 // +----------------------------------------------------------------------
 return [
+
     // 指令定义
     'commands' => [
+        'curd' => 'app\common\command\Curd',
+        'node' => 'app\common\command\Node',
     ],
+
 ];
