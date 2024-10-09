f1d049fc0a8f6b41e322ff13f7763c59f8582823
wolfcode <37436228+wolf-leo@users.noreply.github.com>
2024-10-09 11:12:27 +0800

    Update logviewer.php

--- config/logviewer.php
+++ config/logviewer.php
@@ -1,5 +1,9 @@
 <?php
 return [
+
+    // 日志标题
+    'title'          => 'EasyAdmin8框架日志',
+
     // 默认显示日志应用模块
     'default_module' => 'admin',
 
@@ -10,4 +14,10 @@ return [
         'index',
         'api'
     ],
+
+    // layui css 路径
+    'layui_css_path' => '/static/plugs/layui-v2.x/css/layui.css',
+    // layui js 路径
+    'layui_js_path'  => '/static/plugs/layui-v2.x/layui.js',
+
 ];
