47ef5d9f10e15d6229179cc7e1eba958facfa4d1
wolfcode <37436228+wolf-leo@users.noreply.github.com>
2024-10-28 16:35:06 +0800

    feat(auth): 改用 zTree 替换 layui 的 tree 组件 use zTree for node authorization
    - Replace layui tree with zTree for better performance and flexibility
    - Update auth.js to use zTree for node rendering and checking
    - Add zTree CSS and JS files to project
    
    Signed-off-by: wolfcode <37436228+wolf-leo@users.noreply.github.com>

--- public/static/admin/js/system/auth.js
+++ public/static/admin/js/system/auth.js
@@ -1,89 +1,107 @@
 define(["jquery", "easy-admin"], function ($, ea) {
 
-    var init = {
-        table_elem: '#currentTable',
-        table_render_id: 'currentTableRenderId',
-        index_url: 'system.auth/index',
-        add_url: 'system.auth/add',
-        edit_url: 'system.auth/edit',
-        delete_url: 'system.auth/delete',
-        export_url: 'system.auth/export',
-        modify_url: 'system.auth/modify',
-        authorize_url: 'system.auth/authorize',
-    };
+        var init = {
+            table_elem: '#currentTable',
+            table_render_id: 'currentTableRenderId',
+            index_url: 'system.auth/index',
+            add_url: 'system.auth/add',
+            edit_url: 'system.auth/edit',
+            delete_url: 'system.auth/delete',
+            export_url: 'system.auth/export',
+            modify_url: 'system.auth/modify',
+            authorize_url: 'system.auth/authorize',
+        };
 
-    return {
+        return {
 
-        index: function () {
-            ea.table.render({
-                init: init,
-                cols: [[
-                    {type: "checkbox"},
-                    {field: 'id', width: 80, title: 'ID', searchOp: '='},
-                    {field: 'sort', width: 80, title: '排序', edit: 'text'},
-                    {field: 'title', minWidth: 80, title: '权限名称'},
-                    {field: 'remark', minWidth: 80, title: '备注信息'},
-                    {field: 'status', title: '状态', width: 85, search: 'select', selectList: {0: '禁用', 1: '启用'}, templet: ea.table.switch},
-                    {field: 'create_time', minWidth: 80, title: '创建时间', search: 'range'},
-                    {
-                        width: 250,
-                        title: '操作',
-                        templet: ea.table.tool,
-                        operat: [
-                            'edit',
-                            [{
-                                text: '授权',
-                                url: init.authorize_url,
-                                method: 'open',
-                                auth: 'authorize',
-                                class: 'layui-btn layui-btn-normal layui-btn-xs',
-                            }],
-                            'delete'
-                        ]
-                    }
-                ]],
-            });
-
-            ea.listen();
-        },
-        add: function () {
-            ea.listen();
-        },
-        edit: function () {
-            ea.listen();
-        },
-        authorize: function () {
-            var tree = layui.tree;
-
-            ea.request.get(
-                {
-                    url: window.location.href,
-                }, function (res) {
-                    res.data = res.data || [];
-                    tree.render({
-                        elem: '#node_ids',
-                        data: res.data,
-                        showCheckbox: true,
-                        id: 'nodeDataId',
-                    });
-                }
-            );
-
-            ea.listen(function (data) {
-                var checkedData = tree.getChecked('nodeDataId');
-                var ids = [];
-                $.each(checkedData, function (i, v) {
-                    ids.push(v.id);
-                    if (v.children !== undefined && v.children.length > 0) {
-                        $.each(v.children, function (ii, vv) {
-                            ids.push(vv.id);
-                        });
-                    }
+            index: function () {
+                ea.table.render({
+                    init: init,
+                    cols: [[
+                        {type: "checkbox"},
+                        {field: 'id', width: 80, title: 'ID', searchOp: '='},
+                        {field: 'sort', width: 80, title: '排序', edit: 'text'},
+                        {field: 'title', minWidth: 80, title: '权限名称'},
+                        {field: 'remark', minWidth: 80, title: '备注信息'},
+                        {field: 'status', title: '状态', width: 85, search: 'select', selectList: {0: '禁用', 1: '启用'}, templet: ea.table.switch},
+                        {field: 'create_time', minWidth: 80, title: '创建时间', search: 'range'},
+                        {
+                            width: 250,
+                            title: '操作',
+                            templet: ea.table.tool,
+                            operat: [
+                                'edit',
+                                [{
+                                    text: '授权',
+                                    url: init.authorize_url,
+                                    method: 'open',
+                                    auth: 'authorize',
+                                    class: 'layui-btn layui-btn-normal layui-btn-xs',
+                                }],
+                                'delete'
+                            ]
+                        }
+                    ]],
                 });
-                data.node = JSON.stringify(ids);
-                return data;
-            });
 
+                ea.listen();
+            },
+            add: function () {
+                ea.listen();
+            },
+            edit: function () {
+                ea.listen();
+            },
+            authorize: function () {
+
+                let setting = {
+                    check: {
+                        enable: true,
+                        chkStyle: "checkbox",
+                    },
+                    view: {
+                        showIcon: true,
+                        showLine: true,
+                        selectedMulti: false,
+                        dblClickExpand: false
+                    }, callback: {
+                        onClick: function (e, treeId, treeNode, clickFlag) {
+                            treeObj.checkNode(treeNode, !treeNode.checked, true);
+                        }
+                    }
+                };
+                let treeObj
+                let treeData = []
+                ea.request.get({url: window.location.href}, function (res) {
+                        res.data = res.data || [];
+                        $.each(res.data, function (index, value) {
+                            treeData[index] = []
+                            treeData[index].id = value.id
+                            treeData[index].name = value.title
+                            treeData[index].pId = 0
+                            treeData[index].open = true
+                            let children = value.children
+                            treeData[index]['children'] = []
+                            $.each(children, function (idx, val) {
+                                treeData[index]['children'].push({id: val.id, name: val.title, checked: val.checked, pId: value.id})
+                            })
+                        })
+                        $.fn.zTree.init($("#tree"), setting, treeData);
+                        treeObj = $.fn.zTree.getZTreeObj("tree");
+                    }
+                );
+
+                ea.listen(function (data) {
+                    let checkedData = treeObj.getCheckedNodes();
+                    let ids = []
+                    for (var i = 0; i < checkedData.length; i++) {
+                        ids.push(checkedData[i].id)
+                    }
+                    data.node = JSON.stringify(ids);
+                    return data;
+                });
+
+            }
         }
-    };
-});
+    }
+)
